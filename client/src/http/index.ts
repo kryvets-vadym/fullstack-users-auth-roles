72bacf3b288357ce7e1182365a1cadbdfd754019
postoronnimV <vadimkrvts03@gmail.com>
2023-03-27 01:17:43 +0300

    client url edit

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export function createClient() {
   return axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
   });
 }
